84c08e0c54cbb9118e56505279254ad8043b4033
Eunwoo Jung <jungeu1509@gmail.com>
2021-02-03 15:27:01 +0900

    add Google Search Console

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Edge Case: Many Tags",
+        "excerpt":"This post has many tags.  ","categories": ["Edge Case"],
+        "tags": ["8BIT","alignment","Articles","captions","categories","chat","comments","content","css","dowork","edge case","embeds","excerpt","Fail","featured image","FTW","Fun","gallery","html","image","Jekyll","layout","link","Love","markup","Mothership","Must Read","Nailed It","Pictures","Post Formats","quote","standard","Success","Swagger","Tags","template","title","twitter","Unseen","video","YouTube"],
+        "url": "http://localhost:4000/edge%20case/edge-case-many-tags/",
+        "teaser": null
+      },{
+        "title": "Layout: Comments Enabled",
+        "excerpt":"This post should display comments if a provider is enabled.  ","categories": ["Layout","Uncategorized"],
+        "tags": ["comments","layout"],
+        "url": "http://localhost:4000/layout/uncategorized/layout-comments/",
+        "teaser": null
+      },{
+        "title": "Test",
+        "excerpt":"테스트 게시물 입니다.   블로그 제작 시작 : 2021-02-01   블로그 테스트 : 2021-02-03  ","categories": ["Uncategorized","Test"],
+        "tags": ["test"],
+        "url": "http://localhost:4000/uncategorized/test/test/",
+        "teaser": null
+      }]
